832f47f1f258cdad8ffae9c83ad3501fba8cc741
ufvelouria <ufvelouria@gmail.com>
2025-12-08 11:30:48 -0500

    tried to fix callback links

--- spotify-profile-demo/src/script.js
+++ spotify-profile-demo/src/script.js
@@ -18,7 +18,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://cs226final.vercel.app/");
+    params.append("redirect_uri", "https://cs226final.vercel.app/api/callback");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -54,7 +54,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://cs226final.vercel.app/");
+    params.append("redirect_uri", "https://cs226final.vercel.app/api/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
