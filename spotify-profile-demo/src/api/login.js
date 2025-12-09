5a5b1b0771fd530ad923283bf1ac90da4515fd3a
ufvelouria <ufvelouria@gmail.com>
2025-12-09 11:35:33 -0500

    t

--- spotify-profile-demo/src/api/login.js
+++ spotify-profile-demo/src/api/login.js
@@ -1,31 +1,17 @@
-// api/login.js
 export default function handler(req, res) {
-    const clientId = process.env.SPOTIFY_CLIENT_ID; // use env variables
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
     const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
     const scope = "user-read-private user-read-email user-read-recently-played user-top-read";
 
-    // Generate random code verifier/challenge (PKCE)
-    const verifier = [...Array(128)].map(() => Math.random().toString(36)[2]).join('');
-    const base64url = str => Buffer.from(str).toString('base64')
-        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-
-    const challenge = base64url(
-        require('crypto').createHash('sha256').update(verifier).digest()
-    );
-
-    // Store verifier in cookie (or memory)
-    res.setHeader('Set-Cookie', `verifier=${verifier}; Path=/; HttpOnly`);
-
+    // Simple redirect without PKCE (simpler for Vercel)
     const params = new URLSearchParams({
         client_id: clientId,
-        response_type: 'code',
+        response_type: 'token', // Implicit grant
         redirect_uri: redirectUri,
-        code_challenge_method: 'S256',
-        code_challenge: challenge,
         scope
     });
 
-    // Redirect user to Spotify login
     res.writeHead(302, { Location: `https://accounts.spotify.com/authorize?${params}` });
     res.end();
 }
+
