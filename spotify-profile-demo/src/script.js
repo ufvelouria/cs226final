b128a2b6a82aa952493805bbc89f1a967d165fb6
ufvelouria <ufvelouria@gmail.com>
2025-12-09 13:53:18 -0500

    t

--- spotify-profile-demo/src/script.js
+++ spotify-profile-demo/src/script.js
@@ -6,7 +6,7 @@ const recentTracks = 10;
 
 const topArtists = await fetchTopArtists();
 async function init() {
-    // clearData(); // for testing purposes, clear local storage on each load
+    clearData(); // for testing purposes, clear local storage on each load
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
     const storedToken = localStorage.getItem("access_token");
